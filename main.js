e4bd80d357e31bf7243dec613b10894b5d6814d4
ss7424Refar <812647742@qq.com>
2020-09-30 14:01:48 +0800

    [Update] project structure

--- main.js
+++ main.js
@@ -1,36 +1,52 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow, ipcMain} = require('electron')
+const {app, BrowserWindow, ipcMain, shell, dialog} = require('electron')
+const Store = require('electron-store');
+
 const path = require('path')
+
+let mainWindow = null;
+let secondWindow = null;
+const store = new Store();
+
+require('./server.js')
 
 function createWindow() {
     // Create the browser window.
-    const mainWindow = new BrowserWindow({
-        width: 400,
-        height: 400,
+    mainWindow = new BrowserWindow({
+        // width: 300,
+        // height: 330,
+        // useContentSize: true,
         webPreferences: {
+            nodeIntegration:true,
             preload: path.join(__dirname, 'preload.js'),
             // zoomFactor: 0.75
-            // devTools: false
+            // devTools: true
         },
 
         alwaysOnTop: true,
-        frame: false,
-        resizable: false
+        // frame: false,
+        // resizable: false
     })
+    if ('undefined' !== store.get('window-size')) {
+        mainWindow.setBounds(store.get('window-size'))
+    } else {
+        mainWindow.setBounds({width: 350, height: 350 })
+    }
 
     mainWindow.removeMenu()
 
     // and load the index.html of the app.
-    mainWindow.loadFile('./dist/index.html')
-    // mainWindow.loadFile('index.html')
+    // mainWindow.loadFile('./dist/index.html')
+    mainWindow.loadFile('index.html')
     const options = {extraHeaders: 'pragma: no-cache\n'}
     // mainWindow.loadURL('http://localhost:8080/', options);
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools({mode: 'bottom'})
+    // mainWindow.webContents.openDevTools({mode: 'bottom'})
 
     mainWindow.on('resize', function () {
-        console.log(JSON.stringify(mainWindow.getContentBounds()));
+        console.log('resize ' + JSON.stringify(store.get('window-size')));
+        store.set('window-size', mainWindow.getContentBounds())
     })
 }
 
@@ -56,11 +72,7 @@ app.on('activate', function () {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
-require('./server.js')
-
 ipcMain.on('open-window', (event, arg) => {
     console.log(arg) // prints "ping"
-
-
-
+    shell.openExternal(arg)
 })
--- ui/vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    //输出文件目录
-    outputDir: 'dist',
-    // 判断是否为生产环境，如果是改成/spa/路径
-    publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
-    runtimeCompiler: true,
-}
